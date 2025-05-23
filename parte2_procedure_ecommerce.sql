a36869bb2a7587e0a59a924f931132e038ebea08
Kyndrael <jonathansilveira95@hotmail.com>
2025-05-23 07:38:51 -0300

    Update parte2_procedure_ecommerce.sql

--- parte2_procedure_ecommerce.sql
+++ parte2_procedure_ecommerce.sql
@@ -9,19 +9,25 @@ CREATE PROCEDURE sp_manage_products (
     IN p_price DECIMAL(10, 2)
 )
 BEGIN
-    CASE p_option
-        WHEN 1 THEN
-            INSERT INTO products (id, name, price) VALUES (p_id, p_name, p_price);
-        WHEN 2 THEN
-            UPDATE products SET name = p_name, price = p_price WHERE id = p_id;
-        WHEN 3 THEN
-            DELETE FROM products WHERE id = p_id;
-        WHEN 4 THEN
-            SELECT * FROM products WHERE id = p_id;
-    END CASE;
+    IF p_option = 1 THEN
+        INSERT INTO products (id, name, price) VALUES (p_id, p_name, p_price);
+        
+    ELSEIF p_option = 2 THEN
+        UPDATE products SET name = p_name, price = p_price WHERE id = p_id;
+        
+    ELSEIF p_option = 3 THEN
+        DELETE FROM products WHERE id = p_id;
+        
+    ELSEIF p_option = 4 THEN
+        SELECT * FROM products WHERE id = p_id;
+        
+    ELSE
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Opção inválida. Use 1 (INSERT), 2 (UPDATE), 3 (DELETE) ou 4 (SELECT).';
+    END IF;
 END$$
 
 DELIMITER ;
 
--- Chamada de exemplo:
+-- Chamada de exemplo (UPDATE):
 CALL sp_manage_products(2, 205, 'Teclado Mecânico', 299.99);
