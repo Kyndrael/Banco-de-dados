6251fb70e207efd6ff3721ed357cd12b0ee620f6
Kyndrael <jonathansilveira95@hotmail.com>
2025-05-23 07:39:38 -0300

    Update parte2_procedure_universidade.sql

--- parte2_procedure_universidade.sql
+++ parte2_procedure_universidade.sql
@@ -9,19 +9,30 @@ CREATE PROCEDURE sp_manage_students (
     IN p_course VARCHAR(100)
 )
 BEGIN
-    CASE p_option
-        WHEN 1 THEN -- INSERT
-            INSERT INTO students (id, name, course) VALUES (p_id, p_name, p_course);
-        WHEN 2 THEN -- UPDATE
-            UPDATE students SET name = p_name, course = p_course WHERE id = p_id;
-        WHEN 3 THEN -- DELETE
-            DELETE FROM students WHERE id = p_id;
-        WHEN 4 THEN -- SELECT
-            SELECT * FROM students WHERE id = p_id;
-    END CASE;
+    IF p_option = 1 THEN
+        -- Inserção de novo estudante
+        INSERT INTO students (id, name, course) VALUES (p_id, p_name, p_course);
+    
+    ELSEIF p_option = 2 THEN
+        -- Atualização de estudante existente
+        UPDATE students SET name = p_name, course = p_course WHERE id = p_id;
+    
+    ELSEIF p_option = 3 THEN
+        -- Remoção de estudante
+        DELETE FROM students WHERE id = p_id;
+    
+    ELSEIF p_option = 4 THEN
+        -- Consulta de estudante
+        SELECT * FROM students WHERE id = p_id;
+    
+    ELSE
+        -- Opção inválida
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Opção inválida. Use 1 (INSERT), 2 (UPDATE), 3 (DELETE) ou 4 (SELECT).';
+    END IF;
 END$$
 
 DELIMITER ;
 
--- Chamada de exemplo:
+-- Chamada de exemplo (INSERT):
 CALL sp_manage_students(1, 101, 'Carlos Silva', 'Engenharia');
